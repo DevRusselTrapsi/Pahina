8bfd698aaedfbe6e9f466377d8b0e4efa0d0352c
Trapsi, Markr Russel B. BSIT - 3B <78476407+DevRusselTrapsi@users.noreply.github.com>
2021-11-21 22:25:23 +0800

    added genre selection page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -35,6 +35,10 @@ const routes: Routes = [
     path: 'settings',
     loadChildren: () => import('./pages/settings/settings.module').then( m => m.SettingsPageModule)
   },
+  {
+    path: 'choices',
+    loadChildren: () => import('./choices/choices.module').then( m => m.ChoicesPageModule)
+  },
 
 ];
 
